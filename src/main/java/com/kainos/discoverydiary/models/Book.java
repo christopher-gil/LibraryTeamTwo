f2d719a0eaebf1664fb817f7080bbc60bffcf1ac
conortag <conort@kainos.com>
2016-11-03 14:15:15 +0000

    tdp_1.5_c adding constructor to book and adding book data store

--- src/main/java/com/kainos/discoverydiary/models/Book.java
+++ src/main/java/com/kainos/discoverydiary/models/Book.java
@@ -3,6 +3,10 @@ package com.kainos.discoverydiary.models;
 public class Book {
     private String title;
 
+    public Book(String title) {
+        this.title = title;
+    }
+
     public String getTitle() {
         return title;
     }
