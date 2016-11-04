5142964eae93dc99e76ac2569b6bf9d94a31ef00
Corey Duffy <c.duffy@kainos.com>
2016-11-04 15:12:42 +0000

    Further POST method code

---
+++ src/main/resources/assets/js/advancedSearch.js
@@ -0,0 +1,4 @@
+document.getElementById('authorValue').value
+document.getElementById('apublicationValue').value
+document.getElementById('descriptionValue').value
+document.getElementById('categoryValue').value
