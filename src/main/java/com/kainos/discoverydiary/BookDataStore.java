a756d61d74109475059e328330cc79317f86ebad
Ciara <C.Magee@kainos.com>
2016-11-04 15:06:24 +0000

    Search by category with unit tests tdp_2.3

--- src/main/java/com/kainos/discoverydiary/BookDataStore.java
+++ src/main/java/com/kainos/discoverydiary/BookDataStore.java
@@ -4,21 +4,22 @@ import com.google.common.collect.Lists;
 import com.kainos.discoverydiary.models.Book;
 import org.joda.time.DateTime;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookDataStore {
 
     public void constructBooks() {
-        books.add(new Book(1, "Real World SharePoint 2007", "Hiller", "Development", "Wrox", DateTime.now() ,
+        books.add(new Book(1, "Real World SharePoint 2007", "Technical", "Hiller", "Wrox", DateTime.now(),
                 "SharePoint Guide", "Belfast"));
-        books.add(new Book(2, "Windows Command-Line", "William Stanek", "Development", "MK", DateTime.now(),
+        books.add(new Book(2, "Windows Command-Line", "Non-technical", "William Stanek", "MK", DateTime.now(),
                 "Windows Command Line Guide", "Belfast"));
-        books.add(new Book(3, "Computer Organization And Design", "Patterson & Hennessy", "Development", "O'Reilly",
+        books.add(new Book(3, "Computer Organization And Design", "Technical", "Patterson & Hennessy", "O'Reilly",
                 DateTime.now(), "Discussion of Computer Organisation and Design and the hardware/software interface",
                 "Belfast"));
-        books.add(new Book(4, "Perl Cookbook", "Christiansen & Torkington", "Development", "Mercer", DateTime.now(),
+        books.add(new Book(4, "Perl Cookbook", "Technical", "Christiansen & Torkington", "Mercer", DateTime.now(),
                 "Perl Information and Guidance", "Belfast"));
-        books.add(new Book(5, "Marketing", "Mercer", "Development", "HB", DateTime.now(), "Marketing Treatise",
+        books.add(new Book(5, "Marketing", "Non-Technical", "Mercer", "HB", DateTime.now(), "Marketing Treatise",
                 "Belfast"));
     }
 
@@ -31,4 +32,31 @@ public class BookDataStore {
     public Book getBook(int id) {
         return books.stream().filter(b -> b.getId() == id).findFirst().get();
     }
-}
+
+    public ArrayList<Book> getTechnical(List<Book> allbooks) {
+
+            ArrayList<Book> technical = new ArrayList<>();
+
+            for (Book book: allbooks) {
+                if (book.getCategory().equalsIgnoreCase("Technical")) {
+                    technical.add(book);
+                }
+            }
+            return technical;
+
+        }
+
+    public ArrayList<Book> getNonTechnical(List<Book> allbooks) {
+
+        ArrayList<Book> technical = new ArrayList<>();
+
+        for (Book book: allbooks) {
+            if (book.getCategory().equalsIgnoreCase("Non-Technical")) {
+                technical.add(book);
+            }
+        }
+        return technical;
+
+    }
+    }
+
