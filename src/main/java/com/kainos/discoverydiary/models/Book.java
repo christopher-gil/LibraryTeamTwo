d761050064986d0ec9be768db41dc54f803c390c
Ciara <C.Magee@kainos.com>
2016-11-04 10:25:19 +0000

    Extra fields added to Book object

--- src/main/java/com/kainos/discoverydiary/models/Book.java
+++ src/main/java/com/kainos/discoverydiary/models/Book.java
@@ -1,11 +1,35 @@
 package com.kainos.discoverydiary.models;
 
-public class Book {
-    private String title;
+import org.joda.time.DateTime;
 
-    public Book(String title) {
-        this.title = title;
+public class Book {
+
+    private int id;
+    private String title;
+    private String category;
+    private String author;
+    private String publisher;
+    private DateTime datePublished;
+    private String description;
+    private String location;
+
+    public Book() {
     }
+
+    public Book(int id, String title, String category, String author, String publisher, DateTime datePublished, String description, String location) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.author = author;
+        this.publisher = publisher;
+        this.datePublished = datePublished;
+        this.description = description;
+        this.location = location;
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
 
     public String getTitle() {
         return title;
@@ -14,4 +38,28 @@ public class Book {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public String getCategory() { return category; }
+
+    public void setCategory(String category) { this.category = category; }
+
+    public String getAuthor() { return author; }
+
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getPublisher() { return publisher; }
+
+    public void setPublisher(String publisher) { this.publisher = publisher; }
+
+    public DateTime getDatePublished() { return datePublished; }
+
+    public void setDatePublished(DateTime datePublished) { this.datePublished = datePublished; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public String getLocation() { return location; }
+
+    public void setLocation(String location) { this.location = location; }
 }
