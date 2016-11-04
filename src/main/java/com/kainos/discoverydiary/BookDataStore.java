2008c26e3215bfd0bf1c36c96bdceb92541dc2aa
Anne mcauley <annem@montecristo.local>
2016-11-04 14:15:48 +0000

    add a book tdp_ 6.7

--- src/main/java/com/kainos/discoverydiary/BookDataStore.java
+++ src/main/java/com/kainos/discoverydiary/BookDataStore.java
@@ -2,24 +2,24 @@ package com.kainos.discoverydiary;
 
 import com.google.common.collect.Lists;
 import com.kainos.discoverydiary.models.Book;
+import org.joda.time.DateTime;
 
 import java.util.List;
 
 public class BookDataStore {
-
-    public void constructBooks(){
-        books.add(new Book("Real World SharePoint 2007"));
-        books.add(new Book("Windows Command-Line"));
-        books.add(new Book("Computer Organization And Design"));
-        books.add(new Book("Perl Cookbook"));
-        books.add(new Book("Marketing"));
-    }
 
     private static List<Book> books = Lists.newArrayList();
 
     public List<Book> getBooks() {
         return books;
     }
+
+    public void registerResource(String title, String category, String author, String publisher, DateTime datePublishedToDateTime, String description, String location) {
+        Book book = new Book(title, category, author, publisher, datePublishedToDateTime, description, location);
+        books.add(book);
+    }
+
+
 
 
 
