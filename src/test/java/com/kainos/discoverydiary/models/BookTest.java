4657fb37f7bb51c16fb15fe0bd86f06fbdcdbe0f
conortag <conort@kainos.com>
2016-11-04 09:51:30 +0000

    tdp 1.5 book and datastore test

---
+++ src/test/java/com/kainos/discoverydiary/models/BookTest.java
@@ -0,0 +1,37 @@
+package com.kainos.discoverydiary.models;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by conort on 04/11/2016.
+ */
+public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void setup(){
+        book = new Book();
+    }
+
+    @Test
+    public void bookConstructor_Empty_ReturnsInstanceOfBook(){
+        Assert.assertTrue(new Book() instanceof Book);
+    }
+
+    @Test
+    public void bookConstructor_ValidTitle_ReturnsInstanceOfBook(){
+        Assert.assertTrue(new Book("Valid Title") instanceof Book);
+    }
+
+    @Test
+    public void setterAndGetter_ValidTitle_ReturnsValidTitle(){
+        String title = "Perl Cookbook";
+        book.setTitle(title);
+        Assert.assertEquals(title, book.getTitle());
+    }
+}
